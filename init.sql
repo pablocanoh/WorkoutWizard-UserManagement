642340e73f172ec53fbe70c3def850b3e6c2bb4d
pablocanohueros <canohueros75@gmail.com>
2023-11-05 19:47:31 +0100

    init

--- init.sql
+++ init.sql
@@ -1,8 +1,15 @@
 -- create alert table a load some initial data
 create table user (
     id uuid primary key,
-    name text,
-    email text
+    created_date timestamp default now() NOT NULL,
+    updated_date timestamp default now() NOT NULL,
+    state text NOT NULL,
+    password text NOT NULL,
+    name text NOT NULL,
+    email text NOT NULL,
+
+    UNIQUE (email)
 );
 
-INSERT INTO user (id, name, email) VALUES (uuid(), 'John Doe', 'johndoe@email.com')
+INSERT INTO user (id, state, password, name, email) VALUES ('00000000-0000-0000-0000-000000000000', 'ACTIVE', 'password', 'admin', 'admin@localhost');
+INSERT INTO user (id, state, password, name, email) VALUES ('00000000-0000-0000-0000-000000000001', 'ACTIVE', 'password', 'test user', 'testuser@localhost');
