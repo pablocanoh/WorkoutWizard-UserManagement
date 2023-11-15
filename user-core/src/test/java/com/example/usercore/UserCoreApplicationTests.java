84d9c7a0c9b333cfd09fc3fa2360cd582c8ef637
pablocanohueros <canohueros75@gmail.com>
2023-11-15 22:11:23 +0100

    init

---
+++ user-core/src/test/java/com/example/usercore/UserCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.usercore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
