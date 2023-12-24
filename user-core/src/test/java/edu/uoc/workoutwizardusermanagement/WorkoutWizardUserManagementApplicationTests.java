d1619b8fa90c45ef273c92d77e6670aff4ba79dc
pablocanohueros <canohueros75@gmail.com>
2023-12-24 20:00:19 +0100

    commit

--- user-core/src/test/java/edu/uoc/workoutwizardusermanagement/WorkoutWizardUserManagementApplicationTests.java
+++ user-core/src/test/java/edu/uoc/workoutwizardusermanagement/WorkoutWizardUserManagementApplicationTests.java
@@ -1,13 +1,13 @@
-package edu.uoc.workoutwizardusermanagement;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class WorkoutWizardUserManagementApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//package edu.uoc.workoutwizardusermanagement;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class WorkoutWizardUserManagementApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
